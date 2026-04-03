22cbd1c3bb39e8274dc5f8d9c7ca2dd096c0caaa
Jonathan Gosling <jonathangosling@Jonathans-MacBook-Air.local>
2026-04-03 19:18:21 +0100

    Add Glue Data Quality rulesets for silver and gold tables

---
+++ terraform/glue_dq.tf
@@ -0,0 +1,97 @@
+resource "aws_glue_data_quality_ruleset" "silver_properties" {
+  name = "silver-properties-dq"
+
+  ruleset = <<-DQDL
+    Rules = [
+      RowCount > 500,
+      IsComplete "prop_id",
+      IsComplete "address",
+      IsComplete "latitude",
+      IsComplete "longitude",
+      IsUnique "prop_id",
+      Completeness "postcode" >= 0.9,
+      ColumnValues "area_code" matches "^SW.*" >= 0.9
+    ]
+  DQDL
+
+  target_table {
+    database_name = aws_glue_catalog_database.property_data.name
+    table_name    = "silver_properties"
+  }
+}
+
+resource "aws_glue_data_quality_ruleset" "silver_prices" {
+  name = "silver-prices-dq"
+
+  ruleset = <<-DQDL
+    Rules = [
+      RowCount > 500,
+      IsComplete "prop_id",
+      IsComplete "price",
+      IsComplete "date",
+      ColumnValues "price" between 100 and 50000
+    ]
+  DQDL
+
+  target_table {
+    database_name = aws_glue_catalog_database.property_data.name
+    table_name    = "silver_prices"
+  }
+}
+
+resource "aws_glue_data_quality_ruleset" "silver_spy_prices" {
+  name = "silver-spy-prices-dq"
+
+  ruleset = <<-DQDL
+    Rules = [
+      RowCount > 0,
+      IsComplete "date",
+      IsComplete "close",
+      ColumnValues "close" > 0,
+      IsUnique "date"
+    ]
+  DQDL
+
+  target_table {
+    database_name = aws_glue_catalog_database.property_data.name
+    table_name    = "silver_spy_prices"
+  }
+}
+
+resource "aws_glue_data_quality_ruleset" "gold_current_properties" {
+  name = "gold-current-properties-dq"
+
+  ruleset = <<-DQDL
+    Rules = [
+      RowCount > 400,
+      IsComplete "prop_id",
+      IsComplete "price",
+      IsComplete "area_code",
+      ColumnValues "price" between 100 and 50000
+    ]
+  DQDL
+
+  target_table {
+    database_name = aws_glue_catalog_database.property_data.name
+    table_name    = "gold_current_properties"
+  }
+}
+
+resource "aws_glue_data_quality_ruleset" "gold_property_fact" {
+  name = "gold-property-fact-dq"
+
+  ruleset = <<-DQDL
+    Rules = [
+      RowCount > 0,
+      IsComplete "area_code",
+      IsComplete "avg_price",
+      ColumnValues "avg_price" between 500 and 20000,
+      ColumnValues "num_properties" > 0
+    ]
+  DQDL
+
+  target_table {
+    database_name = aws_glue_catalog_database.property_data.name
+    table_name    = "gold_property_fact"
+  }
+}
