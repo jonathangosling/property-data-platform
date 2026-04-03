0d8960a66b465a280baeb82e26bf6dff9cb72bed
Jonathan Gosling <101741089+jonathangosling@users.noreply.github.com>
2026-04-03 16:08:42 +0100

    Fix extra-py-files: upload src modules individually under src/ prefix (#13)

--- terraform/glue.tf
+++ terraform/glue.tf
@@ -31,7 +31,7 @@ resource "aws_glue_job" "ingest" {
     "--landing_path"              = local.landing_path
     "--secret_name"               = aws_secretsmanager_secret.googlemaps_api_key.name
     "--additional-python-modules" = "requests,beautifulsoup4,yfinance,pandas,s3fs"
-    "--extra-py-files"            = "${local.glue_scripts_path}/shared.zip"
+    "--extra-py-files"            = "${local.glue_scripts_path}/src/scrape.py"
     "--enable-job-insights"       = "false"
   }
 
@@ -52,7 +52,7 @@ resource "aws_glue_job" "ingest_spy" {
   default_arguments = {
     "--landing_path"              = local.landing_path
     "--additional-python-modules" = "yfinance,pandas,s3fs"
-    "--extra-py-files"            = "${local.glue_scripts_path}/shared.zip"
+    "--extra-py-files"            = "${local.glue_scripts_path}/src/financials.py"
     "--enable-job-insights"       = "false"
   }
 
